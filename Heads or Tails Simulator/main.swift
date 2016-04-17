c3796931596c037e3e5ff74902cf7a52d5713378
Daniel Bergquist <daniel.bergquist@gmail.com>
2016-04-17 17:34:19 -0600

    Initial Commit

---
+++ Heads or Tails Simulator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Heads or Tails Simulator
+//
+//  Created by Daniel Bergquist on 4/17/16.
+//  Copyright © 2016 Daniel Bergquist. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
